e237f8b6d61b7a32f51af10e7c58d83347d62f88
soukaMoutaoukil <moutaoukilsoukayna@gmail.com>
2020-09-30 11:32:08 +0200

    update correction schema

--- schemas/correction.js
+++ schemas/correction.js
@@ -3,12 +3,8 @@ const mongoose = require('mongoose');
 const correction  = mongoose.Schema({
     idExercise : String,
     idCorrection : String,
-<<<<<<< HEAD
-    contenu : String
-=======
     idExercise : String,
-    content : String
->>>>>>> 0603121b8cc2c8db225790ad234a8b8c99bd3d78
+    content : String  
 })
 
 module.exports = mongoose.model('correction', correction);
