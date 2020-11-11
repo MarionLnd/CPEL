12d71a3062a1c934cca69db778345ae6c3ca65d8
Soukayna Moutaoukil <soukaynamoutaoukil@MacBook-Pro-de-Soukayna.local>
2020-11-11 14:32:35 +0100

    edit studentRendering

--- schemas/studentRendering.js
+++ schemas/studentRendering.js
@@ -5,6 +5,7 @@ const studentRendering  = mongoose.Schema({
     idExercise : String,
     content : String,
     exerciseDone: Boolean,
+    comment : String
 })
 
 module.exports = mongoose.model('studentRendering', studentRendering);
